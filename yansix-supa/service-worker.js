f9ebfc670e638b9d89ec4b26b54877469ed89363
especialistapele <especialistapele@gmail.com>
2026-08-23 16:35:38 -0300

    Add files via upload
    crm com banco de dados supa

---
+++ yansix-supa/service-worker.js
@@ -0,0 +1,5 @@
+const CACHE="yansix-crm-v5";
+const ASSETS=["./","./index.html","./css/style.css","./js/config.js","./js/api.js","./js/app.js","./assets/yansix-logo.png","./manifest.json"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;if(new URL(e.request.url).origin!==location.origin)return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res})))})
